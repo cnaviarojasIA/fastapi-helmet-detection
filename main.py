e3e9daef9ffc0a2470abbca858b0b85134b68a3c
cnaviarojasIA <cnaviarojasia@gmail.com>
2025-02-25 01:19:58 -0500

    Update main.py
    Se cambia el puerto de 8000 a 8080

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ async def predict(file: UploadFile = File(...)):
     return {"detections": detections}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
